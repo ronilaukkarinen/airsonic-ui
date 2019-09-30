bd6a7e911dc92d1fae5c9213f8f25f634b192d4e
Roni Laukkarinen <roni@dude.fi>
2019-09-30 20:29:52 +0300

    Lastfm tests

--- src/cljs/airsonic_ui/components/audio_player/events.cljs
+++ src/cljs/airsonic_ui/components/audio_player/events.cljs
@@ -1,3 +1,76 @@
+(ns lastfm.client
+  (:require
+    [org.httpkit.client :as http]
+    [clojure.data.json :as json]
+    [clojure.walk :refer [stringify-keys]]))
+
+(def ^:private base-url "http://ws.audioscrobbler.com/2.0/")
+
+(defn ^:private md5
+  "Generate a md5 checksum for the given string"
+  [token]
+  (let [hash-bytes
+         (doto (java.security.MessageDigest/getInstance "MD5")
+               (.reset)
+               (.update (.getBytes token)))]
+       (.toString
+         (new java.math.BigInteger 1 (.digest hash-bytes)) ; Positive and the size of the number
+         16))) ; Use base16 i.e. hex
+
+(defn signed-params [query-params api-secret]
+  (let [params-wo-format (dissoc query-params :format) ;; lastfm api bug. it doesn't include format params in signature
+        sorted-param-string (reduce str (flatten (seq (into (sorted-map) (stringify-keys params-wo-format)))))
+        res-str (str sorted-param-string api-secret)
+        signature (md5 res-str)]
+    (assoc query-params :api_sig signature)))
+
+(defn make-req [{:keys [method params api-secret]}]
+  (let [query-params (assoc params :method method :format "json")
+        p (promise)]
+    (println (signed-params query-params api-secret))
+    (http/get base-url {:query-params (signed-params query-params api-secret)}
+              (fn [{:keys [body error]}]
+                (if error
+                  (deliver p {:error error})
+                  (deliver p {:body (json/read-str body)}))))
+    p))
+
+(defn get-token [api-key api-secret]
+    (let [res @(make-req {:method "auth.getToken" :params {:api_key api-key} :api-secret api-secret})]
+      (get-in res [:body "token"])))
+
+(defn get-session [api-key api-secret token]
+  (let [res @(make-req {:method "auth.getSession" :params {:api_key api-key :token token} :api-secret api-secret})]
+    res))
+
+(defn get-auth-url [api-key token]
+  (str "http://www.last.fm/api/auth/?api_key=" api-key "&token=" token))
+
+(def api-key "ba9c13e15382404dfb046dbbecac3424")
+
+(def api-secret "634c9c84599e82544b86ee2967312967")
+
+(let [token (get-token api-key api-secret)]
+  (get-auth-url api-key token))
+
+;; Your application needs to open a web browser and
+;; send the user to last.fm/api/auth with your API
+;; key and auth token as parameters. Use an HTTP GET
+;; request. Your request will look like this:
+
+;; http://www.last.fm/api/auth/?api_key=xxxxxxxxxxx&token=xxxxxxxx
+;; If the user is not logged in to Last.fm, they will be
+;; redirected to the login page before being asked to grant
+;; your application permission to use their account. On this
+;; page they will see the name of your application, along with
+;; the application description and logo as supplied in Section 1.
+;; Once the user has granted your application permission to use
+;; their account, the browser-based process is over and the user
+;; is asked to close their browser and return to your application.
+
+(get-session api-key api-secret token)
+;; => {:body {"session" {"name" "name", "key" "8482ded6ddf53598391c02a0c2c57b6b", "subscriber" "0"}}}
+
 (ns airsonic-ui.components.audio-player.events
   (:require [re-frame.core :as rf]
             [airsonic-ui.audio.playlist :as playlist]
